e5cf836e22e0c603ff24d8f98dce26c85e1218aa
Jelle Max <jelle.max@dotocean.eu>
2025-12-05 13:48:22 +0100

    Challenge 2

--- day_one/secret_entrance.py
+++ day_one/secret_entrance.py
@@ -1,10 +1,10 @@
+from math import floor
+
 dial = 50
 zero_count = 0
-i = 0
 
 with open("input.txt") as f:
     for x in f:
-        print(dial, x[0], int(x[1:]))
         direction = x[0]
         distance = int(x[1:])
         if direction == 'L':
@@ -12,8 +12,24 @@ with open("input.txt") as f:
         if direction == 'R':
             dial = (dial + distance) % 100
 
-        print(dial)
         if dial == 0:
             zero_count += 1
 
-print(zero_count)
+dial = 50
+zero_pass = 0
+
+with open("input.txt") as f:
+    for line in f:
+
+        direction = line[0]
+        distance = int(line[1:])
+
+        step = -1 if direction == 'L' else 1
+
+        # walk one unit at a time
+        for _ in range(distance):
+            dial = (dial + step) % 100  # wrap 0–99
+            if dial == 0:
+                zero_pass += 1
+
+print(zero_count, zero_pass)
